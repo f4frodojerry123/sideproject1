e4259911c3e9f6d20d01a32dca527df049d461b5
f4frodojerry123 <f4frodojerry123@gmail.com>
2023-05-09 00:17:32 +0800

    commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+let app1 = Vue.createApp({
+    data(){
+      return{
+
+      }
+    },
+    created() {
+
+    },
+    methods:{
+        scroll(){
+            
+        }
+    }
+  });
+  app1.mount(".wrapper");
